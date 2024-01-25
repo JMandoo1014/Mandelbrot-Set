2d1fcd4a9012bef0ec330f146ffdfa682061b1e3
J_Mandoo <90706934+JMandoo1014@users.noreply.github.com>
2024-01-25 21:16:06 +0900

    Update video.py

--- src/video.py
+++ src/video.py
@@ -6,7 +6,7 @@ image_path = "C:/Users/learn-admin/Desktop/UnistPy/images/*.png"
 image_files = sorted(glob.glob(image_path))
 images = [cv2.imread(file) for file in image_files]
 
-video_writer = cv2.VideoWriter("C:/Users/learn-admin/Desktop/UnistPy/images/video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 24, images[0].shape[:2])
+video_writer = cv2.VideoWriter("../images/video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 24, images[0].shape[:2])
 
 for image in images:
     video_writer.write(image)
