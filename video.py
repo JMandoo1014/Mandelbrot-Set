9e3cbaa124535e23afc2d42ec96f0ef853045f2b
learn-admin <learn-admin@unist.ac.kr>
2024-01-25 21:11:23 +0900

    upload

---
+++ video.py
@@ -0,0 +1,16 @@
+import cv2
+import glob
+
+image_path = "C:/Users/learn-admin/Desktop/UnistPy/images/*.png"
+
+image_files = sorted(glob.glob(image_path))
+images = [cv2.imread(file) for file in image_files]
+
+video_writer = cv2.VideoWriter("C:/Users/learn-admin/Desktop/UnistPy/images/video.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 24, images[0].shape[:2])
+
+for image in images:
+    video_writer.write(image)
+
+video_writer.release()
+
+print("video export")
